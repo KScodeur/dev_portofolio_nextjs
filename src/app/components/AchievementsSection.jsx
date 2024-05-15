2c3501dc3e8f898abe0bc4635a887903fab74b52
KScodeur <kinhosamuel2@gmail.com>
2024-05-15 09:29:20 +0000

    Ajout de certains project

--- src/app/components/AchievementsSection.jsx
+++ src/app/components/AchievementsSection.jsx
@@ -11,22 +11,19 @@ const AnimatedNumbers = dynamic(
 
 const achievementsList = [
   {
+    prefix: "~",
     metric: "Projects",
     value: "100",
-    postfix: "+",
   },
   {
     prefix: "~",
     metric: "Users",
     value: "100,000",
   },
-  {
-    metric: "Awards",
-    value: "7",
-  },
+ 
   {
     metric: "Years",
-    value: "5",
+    value: "3",
   },
 ];
 
